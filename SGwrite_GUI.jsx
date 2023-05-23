0ea3ed54b119dd9d4497c4a6d202324e3d978a7a
takowasabi <kmgi45@gmail.com>
2023-05-23 17:12:16 +0900

    GUIひとまず完成。

--- SGwrite_GUI.jsx
+++ SGwrite_GUI.jsx
@@ -1,1 +1,35 @@
-alert("test")
+(function(isPanel) {
+
+    function executeScript(){
+        alert(456456);
+}
+
+    var script_name = 'SGwriteFromAE';
+
+    var win = (isPanel instanceof Panel) ?
+        isPanel :
+        new Window('palette', script_name, undefined, {
+            resizeable: true
+        });
+
+    win.alignChildren = ['fill', 'fill'];
+
+    var Objgroup= win.add('group');
+    Objgroup.alignChildren = ['fill', 'fill'];    
+    Objgroup.orientation = 'row'    
+    Objgroup.spacing=5; 
+    var exeBtn= Objgroup.add('button',undefined,"write ShotGrid");
+    
+    exeBtn.onClick = executeScript;
+
+    win.layout.layout();
+    win.onResize = function() {
+        win.layout.resize();
+    }
+
+    if (!(win instanceof Panel)) {
+        win.center();
+        win.show();
+    }
+
+}(this));
